0a0a0bf75e2873cbb7b7aad82ed99725f8f5c74f
Paul Cheney <spartandesign.biz@gmail.com>
2022-12-16 09:40:32 -0700

    update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -22,3 +22,16 @@ modeBtn.addEventListener('click', () => {
   modeBtn.classList.toggle('dark')
   theMainArea.classList.toggle('dark')
 })
+
+// visit count
+const visitFeedback = document.querySelector('#visitCount');
+let numVisits = localStorage.getItem("visits")
+console.log(numVisits)
+if (numVisits == null) {
+  visitFeedback.textContent = "You're a first timer"
+  localStorage.setItem("visits", 1);
+} else {
+  numVisits++;
+  localStorage.setItem("visits", numVisits);
+  visitFeedback.innerHTML = `You have been here ${numVisits} times &#x1F440;`
+}
