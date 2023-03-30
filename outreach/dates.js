924d2057ee9cc2d635c58af765ca1fc952555dac
Paul Cheney <spartandesign.biz@gmail.com>
2023-03-30 14:28:03 -0600

    update

--- outreach/dates.js
+++ outreach/dates.js
@@ -1,19 +1,21 @@
 /* When does the Block start? */
-const startDay=22
 const startMonth=2
+const startDay=22
 const startYear = 2023
 
 const monthList= ["January","February","March","April","May","June","July",
 "August","September","October","November","December"];
 
 const blockStart = new Date(`${startMonth}/${startDay}/${startYear}`).getTime();
-console.log(blockStart)
+//console.log(blockStart)
 
 function readableDate(add) {
   let y = add * 86400000
-  console.log(y)
+  //console.log(y)
+  
   let x = new Date(blockStart + y )
-  console.log(x)
+  //console.log(x)
+
   m = monthList[x.getMonth()]
   //console.log(m)
 
@@ -26,22 +28,12 @@ function readableDate(add) {
 return `${m} ${d}, ${y}`
 }
 
-/*Build a time stamp*/
+document.querySelector('#endClass').innerHTML = readableDate(43)
 
+document.querySelector('#registerDeadline').innerHTML = readableDate(7)
 
-//console.log('Semester Start Date')
-//console.log(readableDate(5))
-const wk0due = document.querySelector('#week00')
-wk0due.innerHTML = readableDate(0)
+document.querySelector('#week00').innerHTML = readableDate(0)
 
-const wk1due = document.querySelector('#week01')
-wk1due.innerHTML = readableDate(3)
+document.querySelector('#week01').innerHTML = readableDate(3)
 
-const wk2due = document.querySelector('#week02')
-wk2due.innerHTML = readableDate(10)
-
-const endClass = document.querySelector('#endClass')
-endClass.innerHTML = readableDate(43)
-
-const registerDeadline = document.querySelector('#registerDeadline')
-registerDeadline.innerHTML = readableDate(7)
+document.querySelector('#week02').innerHTML = readableDate(10)
