6ac820d7bb2805fb565698ddff087529ec1ef53a
Paul Cheney <spartandesign.biz@gmail.com>
2023-05-08 16:34:00 -0600

    Mod 4

---
+++ home4/scripts/getDates.js
@@ -0,0 +1,5 @@
+document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
+
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
+
